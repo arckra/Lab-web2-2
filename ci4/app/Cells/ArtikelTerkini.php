6ca9762ede25172a53258ad24dc9a2ff1fb2c8d2
arckra <aricakra06@gmail.com>
2026-03-14 18:30:39 +0000

    Mar 15, 2026, 1:30 AM

--- ci4/app/Cells/ArtikelTerkini.php
+++ ci4/app/Cells/ArtikelTerkini.php
@@ -1,16 +1,23 @@
 <?php
+
 namespace App\Cells;
 
-use CodeIgniter\View\Cell;
+use CodeIgniter\View\Cells\Cell;
 use App\Models\ArtikelModel;
 
 class ArtikelTerkini extends Cell
 {
-    public function render()
+    public function render(array $params = []): string
     {
         $model = new ArtikelModel();
-        $artikel = $model->orderBy('created_at', 'DESC')->limit(5)->findAll();
 
-        return view('components/artikel_terkini', ['artikel' => $artikel]);
+        $artikel = $model
+            ->orderBy('created_at', 'DESC')
+            ->limit(5)
+            ->findAll();
+
+        return view('components/artikel_terkini', [
+            'artikel' => $artikel
+        ]);
     }
 }

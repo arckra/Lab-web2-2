c3b72dbc85f83ceade8c47730d189ab45ad86f0f
arckra <aricakra06@gmail.com>
2026-03-30 08:27:34 +0000

    Mar 30, 2026, 3:27 PM

--- ci4/app/Database/Seeds/UserSeeder.php
+++ ci4/app/Database/Seeds/UserSeeder.php
@@ -1,13 +1,15 @@
 <?php
-
 namespace App\Database\Seeds;
-
 use CodeIgniter\Database\Seeder;
-
 class UserSeeder extends Seeder
 {
-    public function run()
-    {
-        //
-    }
+public function run()
+{
+$model = model('UserModel');
+$model->insert([
+'username' => 'admin',
+'useremail' => 'admin@email.com',
+'userpassword' => password_hash('admin123', PASSWORD_DEFAULT),
+]);
+}
 }
